d2bcfb208aaf54e8c6dacf99c1aa84823c443a1b
Xolani-Mxoxozi <37544169+Xolani-Mxoxozi@users.noreply.github.com>
2018-10-10 14:39:54 +0200

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -1,13 +1,13 @@
 # - - coding: utf- 8 - -
 
 # This code prompts a user to enter information and returns it to them
-print "how aold are you?",
+print "how old are you? ",
 age = raw_input()
 
-print "How tall are you?",
+print "How tall are you? ",
 height = raw_input()
 
-print "How much do yopu weight?",
+print "How much do yopu weight? ",
 weight = raw_input()
 
-print "So you're %r old, %r tall and %r heavy." % (age, height, weight)
+print "So you're %r old, %r tall and %r heavy. " % (age, height, weight)
