15f5fc5ac836d3d2b2a4acca296ac5695f0f55f3
Xolani-Mxoxozi <37544169+Xolani-Mxoxozi@users.noreply.github.com>
2018-10-12 12:53:58 +0200

    Update ex21.py

--- ex21.py
+++ ex21.py
@@ -12,6 +12,10 @@ def multiply(a, b):
 	print "MULTIPLYING %d * %d" % (a, b)
 	return a * b
 
+def divide(a, b):
+	14 print "DIVIDING %d / %d" % (a, b)
+	15 return a / b
+
 print "Let's do some math with just functions!"
 
 age = add(30, 5)
